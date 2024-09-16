9b85cbba978a21c43a79c86c722342b0ce93b612
bbrune <barbara.brune@tadi.at>
2024-09-16 14:22:41 +0200

    update simulation example

--- inst/simulation_helpers.R
+++ inst/simulation_helpers.R
@@ -53,6 +53,20 @@ compare_model_fits <- function(d, with_smdm=FALSE) {
     maxit = 20
   )
   
+  hat_weighted_rank <- ranklme(
+    X = d$X, y = d$y, Z = d$Z, g = d$g, 
+    sd_function = "Qn_corrected",
+    adjust_re =TRUE,
+    control_mean_sd = list(
+      mean_function_arguments_fixed = list(),
+      mean_function_arguments_random = list(), 
+      sd_function_arguments_fixed = list(p = (p + d$n.groups)),
+      sd_function_arguments_random = list()),
+    weighted = TRUE, 
+    weighting_type = "hat_matrix",
+    maxit = 20
+  )
+  
   rank <- ranklme(
     X = d$X, y = d$y, Z = d$Z, g = d$g,
     sd_function = "Qn_corrected", adjust_re =TRUE,
@@ -67,13 +81,16 @@ compare_model_fits <- function(d, with_smdm=FALSE) {
   lme <- lmer(f, data = dat)
   
   result_list = list(
-    rank = rank, weighted_rank = weighted_rank, lme = lme
+    rank = rank, 
+    weighted_rank = weighted_rank, 
+    hat_weighted_rank = hat_weighted_rank, 
+    lme = lme
   )
   
   if (with_smdm) {
     koller <- rlmer(f, data = dat)
-    result_list[[4]] <- koller
-    names(result_list)[4] <- "koller"
+    result_list[[5]] <- koller
+    names(result_list)[5] <- "koller"
   }
   
   return(
@@ -117,12 +134,18 @@ get_coefficients_from_model <- function(fitted_object) {
                             variances = c(weighted_rank$sigma, weighted_rank$theta))
   )
   
+  hat_weighted_rank = with(fitted_object,
+                       list(beta = hat_weighted_rank$beta,
+                            variances = c(hat_weighted_rank$sigma, hat_weighted_rank$theta))
+  )
+  
   return(
     data.frame(
       REML = unlist(lme), 
       SMDM = unlist(smdm), 
       rank = unlist(rank), 
       weighted_rank = unlist(weighted_rank), 
+      hat_weighted_rank = unlist(hat_weighted_rank), 
       coef = c("beta_0", "beta_1", "beta_2", "beta_3", "error_variance", "randef_variance1", "randef_variance2"))  
   )
 }
@@ -145,9 +168,10 @@ get_coefficients_from_simulation <- function(fitted_object) {
   ))
   
   if ("koller" %in% names(fitted_object)) {
-    smdm = with(fitted_object,
-                list(beta = fixef(koller),
-                     variances = c(sigma(koller), unlist(lapply(VarCorr(koller), function(z) attr(z, "stddev")))))
+    smdm = unlist(with(fitted_object,
+                       list(beta = fixef(koller),
+                            variances = c(sigma(koller), 
+                                          unlist(lapply(VarCorr(koller), function(z) attr(z, "stddev"))))))
     )
   } else {
     smdm = NULL
@@ -163,8 +187,15 @@ get_coefficients_from_simulation <- function(fitted_object) {
                                    variances = c(weighted_rank$sigma, weighted_rank$theta))
   ))
   
+  hat_weighted_rank = unlist(with(fitted_object,
+                              list(beta = hat_weighted_rank$beta,
+                                   variances = c(hat_weighted_rank$sigma, hat_weighted_rank$theta))
+  ))
   
-  names(lme) <- names(rank) <- names(weighted_rank) <- c("beta_0", "beta_1", "beta_2", "beta_3", "sigma^2", "theta_0", "theta_1")
+  
+  names(lme) <- names(rank) <- names(weighted_rank) <- names(hat_weighted_rank) <-
+    c("beta_0", "beta_1", "beta_2", "beta_3", "sigma^2", "theta_0", "theta_1")
+  
   if ("koller" %in% names(fitted_object)) {
     names(smdm) <- names(lme)
   }
@@ -174,24 +205,28 @@ get_coefficients_from_simulation <- function(fitted_object) {
     result =   dplyr::bind_rows(
       REML = unlist(lme), 
       rank = unlist(rank), 
-      weighted_rank = unlist(weighted_rank) 
+      weighted_rank = unlist(weighted_rank),
+      hat_weighted_rank = unlist(hat_weighted_rank)
     ) |>
       dplyr::mutate(method = c(
         "REML", 
         "rank", 
-        "weighted_rank"))
+        "weighted_rank",
+        "hat_weighted_rank"))
   } else {
     result =   dplyr::bind_rows(
       REML = unlist(lme), 
       SMDM = unlist(smdm), 
       rank = unlist(rank), 
-      weighted_rank = unlist(weighted_rank) 
+      weighted_rank = unlist(weighted_rank),
+      hat_weighted_rank = unlist(hat_weighted_rank)
     ) |>
       dplyr::mutate(method = c(
         "REML", 
         "SMDM",
         "rank", 
-        "weighted_rank"))
+        "weighted_rank",
+        "hat_weighted_rank"))
   }
   
   return(
@@ -215,7 +250,7 @@ get_coefficients_from_simulation <- function(fitted_object) {
 #' A data frame with the algorithms as rows, and names of the coefficients as columns. (output of
 #' `get_coefficients_from_simulation()` function with an extra column for outlier_size.)
 #' 
-replicate_model_fits = function(outlier_size, with_smdm=FALSE) {
+replicate_model_fits = function(outlier_size, predictors = "gaussian", with_smdm=FALSE) {
   
   d <- raw_data(
     n = 200, 
@@ -223,6 +258,7 @@ replicate_model_fits = function(outlier_size, with_smdm=FALSE) {
     k = 2, 
     n.groups = 20, 
     group_size = 10,
+    predictors = predictors,
     ranef_distribution = "normal",
     error_distribution = "normal",
     sd_b = 0.5, 
